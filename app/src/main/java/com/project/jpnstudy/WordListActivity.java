d2ebb8d1c5b26ffc5b9d3bb0967552fee83b207f
jhj0519 <libre@sunmoon.ac.kr>
2018-10-30 11:19:58 +0900

    리스트작성시작

---
+++ app/src/main/java/com/project/jpnstudy/WordListActivity.java
@@ -0,0 +1,19 @@
+package com.project.jpnstudy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WordListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_word_list);
+
+    }
+
+    public class Listview {
+
+         }
+
+}
