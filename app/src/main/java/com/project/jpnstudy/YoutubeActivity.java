d31ca8b0f84213dddc3e4c8612828102e22b3f9a
jhj0519 <libre@sunmoon.ac.kr>
2018-11-01 11:36:14 +0900

    youtube 링크로 연결

---
+++ app/src/main/java/com/project/jpnstudy/YoutubeActivity.java
@@ -0,0 +1,34 @@
+package com.project.jpnstudy;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class YoutubeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_youtube);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        setTitle("youtube 주소 목록");
+        WebView webView = (WebView)findViewById(R.id.y_link);
+        webView.setWebViewClient(new WebViewClient());
+        WebSettings set = webView.getSettings();
+        set.setJavaScriptEnabled(true);
+        set.setBuiltInZoomControls(true);
+
+        webView.loadUrl("https://www.youtube.com/watch?v=BEfUuDt17wc");
+
+
+    }
+
+}
