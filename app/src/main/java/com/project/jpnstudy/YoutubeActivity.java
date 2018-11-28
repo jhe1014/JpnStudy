15043707b94d3118defb860918a11fdc4099afab
jhj0519 <libre@sunmoon.ac.kr>
2018-11-29 01:24:43 +0900

    webpage xml파일 추가 및 수정

--- app/src/main/java/com/project/jpnstudy/YoutubeActivity.java
+++ app/src/main/java/com/project/jpnstudy/YoutubeActivity.java
@@ -1,5 +1,6 @@
 package com.project.jpnstudy;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -8,8 +9,8 @@ import android.view.View;
 import android.widget.LinearLayout;
 
 public class YoutubeActivity extends AppCompatActivity {
-
-   /* public void goToURL(View v) {
+    Toolbar toolbar;
+    public void goToURL(View v) {
         int id = v.getId();
         LinearLayout layout = (LinearLayout)findViewById(id);
         String tag = (String)layout.getTag();
@@ -25,29 +26,16 @@ public class YoutubeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_webpage);
-
-        Intent it = getIntent();
-        String tag = it.getStringExtra("it_tag");
-        WebPage webviwe = (WebPage)findViewById(R.id.y_url);
-        webviwe.
-        set.setJavaScriptEnabled(true);//javascript를 사용할 수 있도록 설정
-        set.setBuiltInZoomControls(true);//안드로이드에서 제공하는 줌 아이콘을 사용할 수 있도록 설정
-
-
-
-
-
-
-
-        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        //setSupportActionBar(toolbar);
-        //setTitle("youtube");
-        //set.setJavaScriptEnabled(true);
-        //set.setBuiltInZoomControls(true);
-
-
-
+        setContentView(android.R.layout.);
     }
 
-}
+
+
+/*
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        setTitle("youtube");
+*/
+    }
+
+
