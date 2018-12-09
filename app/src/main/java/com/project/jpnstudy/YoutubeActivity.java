16a8636b795a6521d419b86ab566141863c67c80
jhe1014 <skana1014@naver.com>
2018-12-09 21:52:00 +0900

    유튜브 액티비티 수정

--- app/src/main/java/com/project/jpnstudy/YoutubeActivity.java
+++ app/src/main/java/com/project/jpnstudy/YoutubeActivity.java
@@ -4,86 +4,104 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.res.TypedArray;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.view.GravityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 
-public class YoutubeActivity extends AppCompatActivity {
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+public class YoutubeActivity extends AppCompatActivity implements AdapterView.OnItemClickListener{
     Toolbar toolbar;
 
+    ArrayList<YoutubeListData> origin_list = new ArrayList<YoutubeListData>();
+    YoutubeListData temp;
     private ListView listview;
-    private YoutubeListAdaptr adapter;
-    private int j;
+    private YoutubeListAdapter adapter;
+
+    private FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+    private DatabaseReference databaseReference = firebaseDatabase.getReference();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.webpage);
+        setContentView(R.layout.activity_youtubelist);
+
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         setTitle("유튜브");
-        adapter = new YoutubeListAdaptr();
-        listview = (ListView) findViewById(R.id.youtube_list);
 
+        adapter = new YoutubeListAdapter(getApplicationContext(), R.id.youtube_list, origin_list);
+        listview = (ListView) findViewById(R.id.youtube_list);
 
         setData();
 
         listview.setAdapter(adapter);
+
+        listview.setOnItemClickListener(this);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        YoutubeListData data = origin_list.get(position);
+
+        Intent intent = new Intent(this, Video.class);
+        intent.putExtra("number", position);
+        startActivity(intent);
     }
 
     private void setData() {
-        String[] y_title = getResources().getStringArray(R.array.y_title);
-        String[] y_about = getResources().getStringArray(R.array.y_data);
+        databaseReference.child("Url").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                adapter.clear();
 
-        for(int i = 0; i < j; i++) {
-            YoutubeitesListData listData = new YoutubeitesListData();
-            listData.sety_title(y_title[i]);
-            listData.sety_data(y_about[i]);
+                Integer rn = 0;
+                while (dataSnapshot.child(rn.toString()).exists()) {
+                    String title = dataSnapshot.child(rn.toString()).child("text").getValue(String.class);
+                    //Log.v("단어", word);
+                    //v_key = dataSnapshot.child(rn.toString()).child("url").getValue(String.class);
+                    //Log.v("뜻", meaning);
 
-            adapter.addItem(listData);
-        }
+                    temp = new YoutubeListData(title);
+                    origin_list.add(temp);
+                    rn = rn + 1;
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 
-
-
-
-
-    public void goToURL(View v) {
-        int id = v.getId();
-        LinearLayout layout = (LinearLayout)findViewById(id);
-        String tag = (String)layout.getTag();
-
-        Intent it = new Intent(this, WebPage.class);
-        it.putExtra("it_tag", tag);
-        startActivity(it);
-    }
-
-
-/*
-    public void goToabout(View v) {
-
-    }
-     Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        setTitle("youtube");
-*/
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) { // 툴바 뒤로가기 버튼
         switch (item.getItemId()) {
             case android.R.id.home: {
                 finish();
-                return true;
+                    return true;
+                }
             }
+            return super.onOptionsItemSelected(item);
         }
-        return super.onOptionsItemSelected(item);
-    }
 
-    }
+
+}
+
 
 
--- app/src/main/java/com/project/jpnstudy/YoutubeListData.java
+++ app/src/main/java/com/project/jpnstudy/YoutubeListData.java
@@ -3,18 +3,13 @@ package com.project.jpnstudy;
 class YoutubeListData {
 ////////////////////////////////////////////////////////////////////////////////////////////////////
     private String y_title;
-    private String y_data;
+
+    public YoutubeListData(String title) {
+        this.y_title = title;
+    }
 
     public String gety_title() {
         return y_title;
     }
-
-    public void sety_title(String word) {
-        y_title = word;
-    }
-
-    public String gety_data() { return y_data; }
-
-    public void sety_data(String meaning) { y_data = meaning; }
 
 }
